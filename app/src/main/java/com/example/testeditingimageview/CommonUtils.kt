5ef0e0a60ee8f7dbf319feb99be49cc195d5abdf
Raja Sibghat ullah <110974113+Sibghat65@users.noreply.github.com>
2023-04-18 16:01:51 +0500

    test2 (#4)
    Co-authored-by: raza saeed <raza.saeed@devgate.com>

--- app/src/main/java/com/example/testeditingimageview/CommonUtils.kt
+++ app/src/main/java/com/example/testeditingimageview/CommonUtils.kt
@@ -1,8 +1,11 @@
 package com.example.testeditingimageview
 
+import android.util.Log
+
 class CommonUtils {
 
     fun getUserData() {
+        Log.d("testdata", "confirm")
         val a = 5
         val b = 10
         val c = a+b
